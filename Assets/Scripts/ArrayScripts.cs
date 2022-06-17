2f14e966c72e6362f73733845d8f167fb7a62f2e
Ashwani Kumar <ashwani.oct8@gmail.com>
2022-06-16 22:03:18 -0500

    creates array

--- Assets/Scripts/ArrayScripts.cs
+++ Assets/Scripts/ArrayScripts.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class ArrayScripts : MonoBehaviour
 {
     // first we declare Array by using  []square brack with the type of the object we wants to create
-    int[] rollNumber; // declare array type
-    string[] studentName; // declare and create array
+    public int[] rollNumber; // declare array of int type, one can declare size either in script of in unity
+    string[] studentName = new string[5] ; // declare and create array of size 5, index from 0 to 4
     
     // Start is called before the first frame update
     void Start()
     {
-        
+       print(rollNumber[2]);// will print the value stored at index number 2, will print 5 for odd numbers stored in array
     }
 
     // Update is called once per frame
